33d0b96ceeebfbd4e790974804b60ccdd6ab00c9
shehab267 <shehabelhawary267@gmail.com>
2024-08-21 18:53:57 +0300

    chore: Update prettier configuration and add dependencies
    - Update .prettierrc.json with new configuration settings
    - Add "@nuxt/ui" and "axios" as dependencies in package.json
    - Add "prettier" and "prettier-plugin-tailwindcss" as devDependencies in package.json
    - Update nuxt.config.ts with runtime configuration, modules, plugins, and color mode settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: '2024-07-30',
+  runtimeConfig: {
+    public: {
+      privateKey: process.env.NUXT_ENV_PRIVATE_KEY,
+      baseUrl: process.env.NUXT_ENV_BASE_URL,
+    },
+  },
+  modules: ["@nuxt/ui"],
+  plugins: [ "@/plugins/axios.ts"],
+  colorMode: {
+    preference: 'light'
+  },
+});
