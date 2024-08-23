77af5737344bdb793426b06032ea5ddde85f1f9f
shehab267 <shehabelhawary267@gmail.com>
2024-08-23 15:26:09 +0300

    chore: Update dependencies & remove unneeded ones

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,14 @@
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-30',
   runtimeConfig: {
     public: {
       privateKey: process.env.NUXT_ENV_PRIVATE_KEY,
       baseUrl: process.env.NUXT_ENV_BASE_URL,
     },
   },
-  modules: ["@nuxt/ui"],
-  plugins: [ "@/plugins/axios.ts"],
-  colorMode: {
-    preference: 'light'
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  imports: {
+    dirs: ['stores']
   },
+  plugins: ["@/plugins/axios.ts"],
 });
--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,24 +1,30 @@
 import axios from "axios";
-
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
   const { privateKey, baseUrl } = config.public;
 
   const axiosInstanceJSON = axios.create({
-    baseURL: baseUrl,
+    baseURL: baseUrl as string,
     headers: {
+      "private-key": privateKey as string,
       "Content-Type": "application/json",
-      "private-key": privateKey,
       Accept: "application/json",
     },
   });
-
+  axiosInstanceJSON.interceptors.request.use((request) => {
+    request.headers["private-key"] = privateKey;
+    return request;
+  });
   axiosInstanceJSON.interceptors.response.use(
-    (response) => response,
+    (response) => {
+      return response;
+    },
     (error) => {
-      if (error.response?.status === 500) {
+      // handled server error globally
+      if (error.response.status === 500) {
         alert("Something went wrong");
       }
+    
       return Promise.reject(error);
     }
   );
